b0b440d57b4f76ec0ddcd8a864cfe2baf8cba7c4
Minliahmetova Violetta <vettaify@gmail.com>
2022-10-26 21:46:53 +0600

    initial commit

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    private String name;
+}
